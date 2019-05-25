c49935d09b20b66c25ba01e6aa6bd73cf1509082
YanWingTsui <47301570+YanWingTsui@users.noreply.github.com>
2019-05-24 22:45:29 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,17 @@
 #include "stack_llist.h"
 #include <vector>
 
-bool conflict(int a, int b, int n, std::vector<std::vector<int>> z){
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < n; j++){
-            if(z[i][j] == 1){
-                //check if (a,b) are in horizontal/vertical/diagnol of (i,j)
-                if(a == i || b == j)
+bool conflict(int x, int y, int n, std::vector<std::vector<int>> z)
+{
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            if(z[i][j] == 1)
+            {
+                if(x == i || y == j)
                     return true;
-                //if slope is 1 or -1 they are diagnol
-                if((a-i)/(b-j) == 1 || (a-i)/(b-j) == -1)
+                if(x-i)/(y-j) == -1 || (x-i)/(y-j) == 1)
                     return true;
             }
         }
@@ -17,51 +19,59 @@ bool conflict(int a, int b, int n, std::vector<std::vector<int>> z){
     return false;
 }
 
-void nQueens(int n){
-    std::vector<std::vector<int>> board;
-    for(int i = 0; i < n; i++){
-        std::vector<int> a(n, 0);
-        board.push_back(a);
+void Q(int n){
+    std::vector<std::vector<int>> BT;
+    for(int a = 0; a < n; a++)
+    {
+        std::vector<int> x(n, 0);
+        BT.push_back(x);
     }
-    //created the board and put in first queen at (0,0)
-    board[0][0] = 1;
+    
+    BT[0][0] = 1;
+    
     nodespace::SList stakx;
     nodespace::SList staky;
+    
     stakx.push(0);
     staky.push(0);
+    
     bool success = false;
-    //program will run until n queens are placed or until stack is empty which means n queens is not possible like in a 2x2 board
-    while(!success && !stakx.empty()){
+    while(!success && !stakx.empty())
+    {
         bool gotThru = false;
-        //places a queen at the stack.size row at column i
-        for(int i = 0; i < n; i++){
-            //checks to see if any of the spots in that column cause conflict, if not then it places down a queen and skips back to start of while
-            if(conflict(stakx.size(), i, n, board) == false){
-                board[stakx.size()][i] = 1;
+        for(int c = 0; c < n; c++)
+        {
+            if(conflict(stakx.size(), c, n, BT) == false)
+            {
+               BT[stakx.size()][c] = 1;
                 stakx.push(stakx.size());
-                staky.push(i);
+                staky.push(c);
                 gotThru = true;
                 break;
             }
         }
-        //if all positions in the column cause conflict then we go back to the position of the last queen
+    
         if(!gotThru){
-            bool retry = true;
-            while(retry && !stakx.empty()){
-                //i take out the last position from the stack
+            bool try = true;
+            while(try && !stakx.empty())
+            {
                 int lastx = stakx.top();
                 int lasty = staky.top();
-                board[lastx][lasty] = 0;
+                
+                BT[lastx][lasty] = 0;
                 stakx.pop();
                 staky.pop();
-                //i check to see if we can shift right, if not i go back to the beginning of this while loop and repeat until a place is found or stack is empty
-                if(lasty < n-1){
-                    for(int i = lasty+1; i < n; i++){
-                        if(conflict(stakx.size(), i, n, board) == false){
-                            board[stakx.size()][i] = 1;
+               
+                if(lasty < n-1)
+                {
+                    for(int d = lasty+1; d < n; d++)
+                    {
+                        if(conflict(stakx.size(), d, n, BT) == false)
+                        {
+                            BT[stakx.size()][d] = 1;
                             stakx.push(stakx.size());
-                            staky.push(i);
-                            retry = false;
+                            staky.push(d);
+                            try = false;
                             break;
                         }
                     }
