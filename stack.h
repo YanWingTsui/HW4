f2a37b1aeaf742a1e2b7481b9ffb3b175dd08dc8
YanWingTsui <47301570+YanWingTsui@users.noreply.github.com>
2019-05-24 22:37:23 -0400

    Update and rename llist.h to stack.h

--- stack.h
+++ stack.h
@@ -16,27 +16,34 @@ namespace nodespace{
             tail_ptr=NULL;
             list_length=0;
         }
+	void push(const node::value_type& entry);
+	int top();
+	void pop();
+	const std::size_t size();
+	void Printing();
+	void EMPTY();
+	
         // functions modified from the linked list toolkit
-        std::size_t length(const node* head_ptr){return list_length;}
+        //std::size_t length(const node* head_ptr){return list_length;}
         
         //add to end, insert at position P
-        void append(const node::value_type& entry);
-        void insert(std::size_t position, const node::value_type& entry);   
+        //void append(const node::value_type& entry);
+       // void insert(std::size_t position, const node::value_type& entry);   
         
         //find first occurrence
-        const node* search(const node::value_type& target);
-        const node* locate(std::size_t position);
+        //const node* search(const node::value_type& target);
+        //const node* locate(std::size_t position);
         
         //remove by value, remove all
-        bool remove(const node::value_type& target);
-        void clear();
+       // bool remove(const node::value_type& target);
+       // void clear();
         
         //returns new list
-        LList* copy();
+      //  LList* copy();
 
         //additional functions to index into list and overwrite node
-        node* operator[] (size_t ind);
-        node& operator = (node::value_type i);
+        //node* operator[] (size_t ind);
+      //  node& operator = (node::value_type i);
 
     private:
         node* head_ptr;
@@ -44,7 +51,7 @@ namespace nodespace{
         size_t list_length;
     };
 
-std::list<std::tuple<node::value_type, int>> frequency(LList data);
+//std::list<std::tuple<node::value_type, int>> frequency(LList data);
 }
 #endif
 
